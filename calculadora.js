257293792eab85cd87d528c1ee5acb5fad40bf69
Mateus Adamo <adamomateus478@gmail.com>
2023-09-07 23:57:06 -0300

    Módulos NodeJs

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+function somar(num1, num2) {
+    return  num1 + num2
+}
+
+function subtrair(num1, num2) {
+    return num1 - num2
+}
+
+function multiplicar(num1, num2) {
+    return num1 * num2
+}
+
+function dividir(num1, num2) {
+    return num1 / num2
+}
+
+
+console.log(module.exports = {somar, subtrair, multiplicar, dividir})
